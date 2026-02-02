5fbb4a54868d2bc9a49a8628cd7ec4e13a384998
Bryan Vela <bdvela20@gmail.com>
2026-02-02 02:32:44 -0500

    chore: remove github pages config and workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  osite: 'https://bdvela.github.io',
+
   i18n: {
     defaultLocale: "es",
     locales: ['es', 'en'],
